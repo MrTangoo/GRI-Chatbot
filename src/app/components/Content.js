856dd6e6302b1a51da506b8a1c86d6a3700e36b3
Maxime Derbigny <maxime.derbigny@gri.ch>
2025-03-13 13:00:28 +0100

    responsive

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -1,11 +1,16 @@
 const Content = () => {
     return (
-        <div className="pt-25">
+        <div className="pt-25 md:pt-25 px-4">
             <div className="text-center">
-                <h1 className="text-9xl font-bold bg-linear-to-r from-gray-600 via-blue-500 to-cyan-400 inline-block text-transparent bg-clip-text pb-2">GRI Chatbot</h1>
-                <p className="text-xl text-gray-500">Un chatbot pour vous aider à trouver des informations sur le GRI </p>
+                <h1 className="text-5xl md:text-6xl lg:text-9xl font-bold bg-gradient-to-r from-gray-600 via-blue-500 to-cyan-400 inline-block text-transparent bg-clip-text pb-2">
+                    GRI Chatbot
+                </h1>
+                <p className="text-base md:text-xl text-gray-500">
+                    Un chatbot pour vous aider à trouver des informations sur le GRI
+                </p>
             </div>
         </div>
     );
 };
+
 export default Content;
--- src/app/components/Description.js
+++ src/app/components/Description.js
@@ -1,9 +1,9 @@
 const Description = () => {
     return (
-        <div className="pt-20 w-1/2 mx-auto">
-            <h1 className="text-center text-4xl font-bold">Soulager le support IT</h1>
-            <p className="text-justify text-xl pt-4">
-                Ce projet a été réalisé dans le cadre du module 245, ayant pour objectif de résoudre une problématique métier grâce à une solution innovante. J'ai donc choisi de développer un chatbot capable de reduire la charge de travail du support, qui fait face à un grand nombre de demandes répétitives. Ce bot vise à automatiser certaines tâches et à améliorer l'efficacité du support.
+        <div className="pt-20 md:pt-20 w-11/12 md:w-3/4 lg:w-1/2 mx-auto">
+            <h1 className="text-center text-2xl md:text-4xl font-bold">Soulager le support IT</h1>
+            <p className="text-justify text-base md:text-xl p-6">
+                Ce projet a été réalisé dans le cadre du module 245, ayant pour objectif de résoudre une problématique métier grâce à une solution innovante. J'ai donc choisi de développer un chatbot capable de réduire la charge de travail du support, qui fait face à un grand nombre de demandes répétitives. Ce bot vise à automatiser certaines tâches et à améliorer l'efficacité du support.
             </p>
         </div>
     );
