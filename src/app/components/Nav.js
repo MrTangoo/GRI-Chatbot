cb5575e3ffb7f8bd42ee670a3d8871312b13a693
Maxime Derbigny <maxime.derbigny@gmail.com>
2025-03-17 09:19:41 +0100

    Fix logo

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,17 +1,26 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Nav = () => {
     return (
         <nav className="p-5 border-b border-gray-300 backdrop-blur-xl">
             <ul className="flex justify-left">
                 <li className="text-2xl">
-                    <Image
-                        src="/logo.png"
-                        alt="Logo GRI"
-                        width={90}
-                        height={90}
-                        priority
-                    />
+                    <motion.div
+                        initial={{ opacity: 0, y: 0 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, ease: 'easeOut' }}
+                    >
+                        <Image
+                            src="/logo.png"
+                            alt="Logo GRI"
+                            width={90}
+                            height={90}
+                            priority
+                        />
+                    </motion.div>
                 </li>
             </ul>
         </nav>
