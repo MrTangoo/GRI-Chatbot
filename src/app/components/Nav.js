e526e64acfa0e0c225ed28157d5701227b5b5115
Maxime Derbigny <maxime.derbigny@gmail.com>
2025-03-17 12:33:45 +0100

    fix load logo

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,16 +1,25 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 const Nav = () => {
+    // État pour vérifier si l'image est chargée
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+    // Fonction qui se déclenche lorsque l'image est chargée
+    const handleImageLoad = () => {
+        setIsImageLoaded(true);
+    };
+
     return (
         <nav className="p-5 border-b border-gray-300 backdrop-blur-xl">
             <ul className="flex justify-left">
                 <li className="text-2xl">
                     <motion.div
                         initial={{ opacity: 0, y: 0 }}
-                        animate={{ opacity: 1, y: 0 }}
+                        animate={{ opacity: isImageLoaded ? 1 : 0, y: 0 }}
                         transition={{ duration: 0.5, ease: 'easeOut' }}
                     >
                         <Image
@@ -19,6 +28,7 @@ const Nav = () => {
                             width={90}
                             height={90}
                             priority
+                            onLoad={handleImageLoad} // Appeler handleImageLoad quand l'image est complètement chargée
                         />
                     </motion.div>
                 </li>
