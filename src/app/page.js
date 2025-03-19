e048b0bebd928cee3a18282060bc3eca319952d0
Maxime Derbigny <maxime.derbigny@gri.ch>
2025-03-19 10:07:56 +0100

    fix responsive height

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ import Description from "./components/Description";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 min-h-screen flex flex-col">
       <Nav />
       <Content />
       <Description />
-      <div className="pt-20">
+      <div className="pt-20 mt-auto">
         <Bot />
       </div>
     </div>
